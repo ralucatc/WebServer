261d6541deba08b963235f6a79e8b6e58a038203
Raluca <carare.raluca27@gmail.com>
2021-11-03 19:09:28 +0200

    some tests

--- app.test.js
+++ app.test.js
@@ -1,8 +1,24 @@
 import supertest from "supertest";
 import app from "./app.js";
 
-test("Should return 404 when Maintenance Mode", () => {
-  const response = request(app).post("/maintenance".send);
+describe("Running Endpoints", () => {
+  test("GET /running should open first page", async () => {
+    const res = await requestWithSupertest.get("/running");
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining("a.html"));
+  });
 });
 
-test();
+describe("Maintenance Endpoints", () => {
+  test("GET /maintenance should return 404", async () => {
+    const res = await requestWithSupertest.get("/maintenance");
+    expect(res.status).toEqual(404);
+  });
+});
+
+describe("Stopped Endpoints", () => {
+  test("GET /stopped should return timeout", async () => {
+    const res = await requestWithSupertest.get("/stopped");
+    expect(res.status).toEqual(404);
+  });
+});
